699a94a13ebf56b5f5024d94938aa6bd556e449f
shcard <shcard16>
2016-07-21 14:48:12 +0900

    push in mac

--- app/src/main/java/com/example/user/testserver/MainActivity.java
+++ app/src/main/java/com/example/user/testserver/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.*;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
