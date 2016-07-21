e5d995eb471bbd529f376e670f4a889520ce3429
jushin17 <jushin17@gmail.com>
2016-07-21 14:24:18 +0900

    git test

---
+++ app/src/main/java/com/example/user/testserver/QuickstartPreferences.java
@@ -0,0 +1,10 @@
+package com.example.user.testserver;
+
+/**
+ * Created by user on 2016-07-21.
+ */
+public class QuickstartPreferences {
+        public static final String REGISTRATION_READY = "registrationReady";
+        public static final String REGISTRATION_GENERATING = "registrationGenerating";
+        public static final String REGISTRATION_COMPLETE = "registrationComplete";
+}
